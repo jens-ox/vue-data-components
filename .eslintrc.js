bd84efbb0d778509b4665e7a90826f9b72586b0f
Jens Ochsenmeier <github@jens-ox.de>
2018-10-21 20:14:42 +0200

    allow padded blocks

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
+    'padded-blocks': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
